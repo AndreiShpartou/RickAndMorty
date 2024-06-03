4eb6cb33d8dec37829fa8e829f5877b22dfa80d7
AndreiShpartou <AndreiShpartou@gmail.com>
2024-06-03 12:10:39 +0300

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Andrei Shpartou on 03/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
