35aa8f57217cc69af604755d9876004eaa1a5f28
AndreiShpartou <AndreiShpartou@gmail.com>
2024-06-04 11:51:52 +0300

    create basic data models

---
+++ RickAndMorty/Models/RMCharacterGender.swift
@@ -0,0 +1,15 @@
+//
+//  RMCharacterGender.swift
+//  RickAndMorty
+//
+//  Created by Andrei Shpartou on 04/06/2024.
+//
+
+import Foundation
+
+enum RMCharacterGender: String, Codable {
+    case female = "Female"
+    case male = "Male"
+    case genderless = "Genderless"
+    case unknown = "unknown"
+}
---
+++ RickAndMorty/Models/RMSingleLocation.swift
@@ -0,0 +1,13 @@
+//
+//  RMSingleLocation.swift
+//  RickAndMorty
+//
+//  Created by Andrei Shpartou on 04/06/2024.
+//
+
+import Foundation
+
+struct RMLastLocation: Codable {
+    let name: String
+    let url: String
+}
