2282992a8cfbc7e0824090c9dac5cb2b45f955f4
AndreiShpartou <AndreiShpartou@gmail.com>
2024-06-05 19:54:14 +0300

    adjust RMCharacterViewCell, create image fetching

---
+++ RickAndMorty/ViewModels/RMCharacterCollectionViewCellViewModel.swift
@@ -0,0 +1,47 @@
+//
+//  RMCharacterCollectionViewCellViewModel.swift
+//  RickAndMorty
+//
+//  Created by Andrei Shpartou on 05/06/2024.
+//
+
+import Foundation
+
+final class RMCharacterCollectionViewCellViewModel {
+    public let characterName: String
+    private let characterStatus: RMCharacterStatus
+    private let characterImageUrl: URL?
+    public var characterStatusText: String {
+        return characterStatus.rawValue
+    }
+    
+    // MARK: - Init
+    init(
+        characterName: String,
+        characterStatus: RMCharacterStatus,
+        characterImageUrl: URL?
+    ) {
+        self.characterName = characterName
+        self.characterStatus = characterStatus
+        self.characterImageUrl = characterImageUrl
+    }
+    
+    public func fetchImage(completion: @escaping (Result<Data, Error>) -> Void) {
+        // TODO: Abstract to Image Manager
+        guard let url = characterImageUrl else {
+            completion(.failure(URLError(.badURL)))
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request, completionHandler: { data, _, error in
+            guard let data = data, error == nil else {
+                completion(.failure(error ?? URLError(.badServerResponse)))
+                return
+            }
+            completion(.success(data))
+        })
+        task.resume()
+    }
+    
+}
