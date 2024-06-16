39ee38d28d840a371b1defd99cfb4783dcfdc0b2
AndreiShpartou <AndreiShpartou@gmail.com>
2024-06-16 19:08:36 +0300

    complete location pagination

--- RickAndMorty/ViewModels/Search/RMSearchResultViewModel.swift
+++ RickAndMorty/ViewModels/Search/RMSearchResultViewModel.swift
@@ -7,8 +7,77 @@
 
 import Foundation
 
-enum RMSearchResultViewModel {
+enum RMSearchResultType {
     case characters([RMCharacterCollectionViewCellViewModel])
     case episodes([RMCharacterEpisodeCollectionViewCellViewModel])
     case locations([RMLocationTableViewCellViewModel])
 }
+
+final class RMSearchResultViewModel {
+    
+    public var shouldShowLoadMoreIndicator: Bool {
+        return next != nil
+    }
+    
+    public private(set) var isLoadingMoreResults = false
+    
+    public private(set) var results: RMSearchResultType
+    
+    private var next: String?
+    
+    // MARK: - Init
+    init(results: RMSearchResultType, next: String? = nil) {
+        self.results = results
+        self.next = next
+    }
+    
+    public func fetchAdditionalLocations(completion: @escaping ([RMLocationTableViewCellViewModel]) -> Void) {
+        isLoadingMoreResults = true
+        guard let urlString = next,
+              let url = URL(string: urlString),
+              let request = RMRequest(url: url) else {
+            print("Failed to create request")
+            isLoadingMoreResults = false
+            return
+        }
+        
+        RMService.shared.execute(
+            request,
+            expecting: RMGetAllLocationsResponse.self,
+            completion: { [weak self] result in
+                switch result {
+                case .success(let responseModel):
+                    let moreResults = responseModel.results
+                    self?.next = responseModel.info.next
+
+                    let additionalLocations = moreResults.compactMap {
+                        return RMLocationTableViewCellViewModel(location: $0)
+                    }
+                    
+                    var newResults: [RMLocationTableViewCellViewModel] = []
+                    switch self?.results {
+                    case .locations(let existingLocations):
+                        newResults = existingLocations + additionalLocations
+                        self?.results = .locations(newResults)
+                    case .episodes, .characters:
+                        break
+                    case .none:
+                        break
+                    }
+                    
+                    DispatchQueue.main.async {
+                        self?.isLoadingMoreResults = false
+                        // Notify via callback
+                        completion(newResults)
+                    }
+
+                    print("More locations: \(moreResults.count)")
+                case .failure(let failure):
+                    print(String(describing: failure))
+                    self?.isLoadingMoreResults = false
+                }
+            }
+        )
+    }
+}
+
